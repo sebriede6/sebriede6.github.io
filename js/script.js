8e2f558b54191454333de207b679d9ba45608e50
sebriede6 <sebriede6@gmail.com>
2024-12-11 16:19:20 +0100

    added zoomfunction and smooth of zoom

---
+++ js/script.js
@@ -0,0 +1,9 @@
+const myImage = document.querySelector('.responsive-image');
+
+myImage.addEventListener('mouseover', () => {
+  myImage.style.transform = 'scale(1.5) translate(50px, 50px)'; 
+});
+
+myImage.addEventListener('mouseout', () => {
+  myImage.style.transform = 'scale(1) translate(0, 0)'; 
+});
